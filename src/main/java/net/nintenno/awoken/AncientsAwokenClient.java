b4bb89b5690d863b789745769961bc5c4ce98aba
smart <smartcampisikid@gmail.com>
2024-05-25 10:16:36 -0500

    Got the basic footprint

---
+++ src/main/java/net/nintenno/awoken/AncientsAwokenClient.java
@@ -0,0 +1,10 @@
+package net.nintenno.awoken;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientsAwokenClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
